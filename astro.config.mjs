596628ef3811fc3c342982a6b3af6d97befaf4a9
Carlos Castellanos <me@karloscodes.com>
2025-05-21 20:47:20 +0000

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
 
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [
+		tailwind(),
+		sitemap({
+			changefreq: "weekly",
+			priority: 0.7,
+			lastmod: new Date(),
+		}),
+	],
+	site: "https://karloscodes.com",
 });
